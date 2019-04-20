01730fc6f411db167aa3cb1cebb3a43b78ee745e
ngtodorov <nt4505@gmail.com>
2019-04-20 11:50:30 -0400

    Current stable version

--- ExpenseLog/Scripts/custom/customscripts.js
+++ ExpenseLog/Scripts/custom/customscripts.js
@@ -35,54 +35,80 @@
     });
 
 
+        $('#formExpenseRecord').validate({
+            rules: {
+                ExpensePrice:   {required: true},
+                ExpenseDate:    {required: true}
+            },
+            messages: {
+                ExpensePrice: { required: "Expense Price is required!"},
+                ExpenseDate: { required: "Expense Date is required!"}
+            }
+        });
+
+
     //--- This script will be executed on Expense Record Create and Edit form submission
     //--- it will first upload the files (attachments) and then it will submit the form 
     $("#buttonSubmit").click(function (event) {
-        var file = $("#file")[0];
-        if (file != null && file.files != null && file.files.length > 0) {
-            //stop submit the form, we will post it manually.
-            event.preventDefault(); // <------------------ stop default behaviour of button -<<< STOP FORM SUBMIT >>>
+        if ($("#formExpenseRecord").valid()) {
+            var file = $("#file")[0];
+            if (file != null && file.files != null
+                && file.files.length > 0) {
+                //stop submit the form, we will post it manually.
+                event.preventDefault(); // <------------------ stop default behaviour of button -<<< STOP FORM SUBMIT >>>
 
-            // disable buttons
-            $("#buttonSubmit").prop("disabled", true);
-            $("#buttonCancel").prop("disabled", true);
+                
+                $("#FilesLabel").val('');
 
-            var attachmentUploadWebAPIUrl = $('#attachmentUploadWebAPIUrl').val();
+                // disable buttons
+                
+                $("#buttonSubmit").prop("disabled", true);
+                //$("#buttonCancel").removeAttr('href');
+                $('#buttonCancel').each(function () {$(this).data("href", $(this).attr("href")).removeAttr("href");});
+                $('#buttonDelete').each(function () {$(this).data("href", $(this).attr("href")).removeAttr("href");});
 
-            var form = $('#formExpenseRecord')[0];
-            //var formData = $('#formExpenseRecord').serialize();
-            var formData = new FormData(form);
+                // WebAPI Url
+                var attachmentUploadWebAPIUrl = $('#attachmentUploadWebAPIUrl').val();
 
-            //--- 1st - Upload the attachments
-            $.ajax({
-                type: 'POST',
-                async: true,
-                url: attachmentUploadWebAPIUrl,
-                data: formData,
-                enctype: 'multipart/form-data',
-                cache: false,
-                contentType: false,
-                processData: false,
-                timeout: 600000,
-                dataType: 'json',
-                success: function (data, textStatus, xhr) {//the "data" object contains the data returned from the server
-                    var statusCode = xhr.status;
-                    if (statusCode == "200" && data != null) {
-                        //--- remove the files because we already uploaded them thru the ajax call of WebAPI
-                        $("#file").val('');
-                        //--- add attachments info to formData
-                        var jsonData = JSON.stringify(data);
-                        $("#attachmentsJson").val(jsonData);
-                        //--- 2nd - Submit the form
-                        $('#formExpenseRecord').submit(); //<------------ submit form
+                var form = $('#formExpenseRecord')[0];
+                //var formData = $('#formExpenseRecord').serialize();
+                var formData = new FormData(form);
+
+                //--- 1st - Upload the attachments
+                $.ajax({
+                    type: 'POST',
+                    async: true,
+                    url: attachmentUploadWebAPIUrl,
+                    data: formData,
+                    enctype: 'multipart/form-data',
+                    cache: false,
+                    contentType: false,
+                    processData: false,
+                    timeout: 600000,
+                    dataType: 'json',
+                    success: function (data, textStatus, xhr) {//the "data" object contains the data returned from the server
+                        var statusCode = xhr.status;
+                        if (statusCode == "200" && data != null) {
+                            //--- remove the files because we already uploaded them thru the ajax call of WebAPI
+                            $("#file").val('');
+                            $("#FilesLabel").val('Submitting the form, please wait ...');
+                            //--- add attachments info to formData
+                            var jsonData = JSON.stringify(data);
+                            $("#attachmentsJson").val(jsonData);
+                            //--- 2nd - Submit the form
+                            $('#formExpenseRecord').submit(); //<------------ submit form
+                        }
+                    },
+                    error: function (jqXHR, textStatus, errorThrown) {
+                        
+                        $('#buttonCancel').each(function () {$(this).attr("href", $(this).data("href"));});
+                        $('#buttonDelete').each(function () {$(this).attr("href", $(this).data("href"));});
+
+                        alert("Error on file upload.\n\nStatus Code: " + jqXHR.status + "\n\nStatus Text: " + jqXHR.statusText + "\n\nMessage: " + jqXHR.responseText);
                     }
-                },
-                error: function (jqXHR, textStatus, errorThrown) {
-                    $("#buttonCancel").prop("disabled", false);
-                    alert("Error on file upload.\n\nStatus Code: " + jqXHR.status + "\n\nStatus Text: " + jqXHR.statusText + "\n\nMessage: " + jqXHR.responseText);
-                }
 
-            });
+                });
+            }
         }
         return true;
     });
